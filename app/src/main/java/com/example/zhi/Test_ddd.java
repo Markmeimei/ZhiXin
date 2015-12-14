be9026735bebba6e7faf3d140ac3fbca95433a32
Eron <w_xuanlong@163.com>
2015-12-14 16:59:21 +0800

    Test

--- app/src/main/java/com/example/zhi/Test_ddd.java
+++ app/src/main/java/com/example/zhi/Test_ddd.java
@@ -1,14 +1,16 @@
 package com.example.zhi;
 
+import android.app.Activity;
 import android.widget.TextView;
 
 /**
  * Author:Eron
  * on 2015/12/14 0014
  */
-public class Test_ddd {
+public class Test_ddd extends Activity{
     private TextView test;
     /**
      * 2323
      */
+
 }
