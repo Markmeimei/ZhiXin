667214ac20cb81005ec835b9b5d8da1b4594f081
Eron <w_xuanlong@163.com>
2016-03-28 23:53:50 +0800

    2016-03-28 23：53 添加任务详情模块

---
+++ app/src/main/java/com/example/zhi/activity/daily/task/TaskDetailsActivity.java
@@ -0,0 +1,22 @@
+package com.example.zhi.activity.daily.task;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 任务详情页
+ *
+ * Author: Eron
+ * Date: 2016/3/28
+ * Time: 21:49
+ */
+public class TaskDetailsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+
+    }
+}
